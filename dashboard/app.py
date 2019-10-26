47871895ada4781c0d3dcf1ba0d427bb028c1c96
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-10-26 18:19:03 -0200

    before psor crash

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 from modulos.dockerm import docker
 
 app = Flask(__name__)
--- dashboard/modulos/dockerm.py
+++ dashboard/modulos/dockerm.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import render_template, Blueprint
+from docker import DockerClient
 
-docker = Blueprint("docker_routes", __name__,url_prefix="/docker")
+docker = Blueprint("docker_routes", __name__, url_prefix="/docker")
+con = DockerClient("tcp://127.0.0.1:2376")
 
 @docker.route("")
 def get_containers():
+    containers = con.containers.list(all=True)
+    print(containers)
     return render_template("docker.html")
