5e8e430a5ba40e0a8d7669bb3962631c05400670
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-11-09 11:37:53 -0200

    added comments

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,7 +1,11 @@
+#ESTE É O SCRIPT QUE STARTA A APLICAÇÃO WEB (EM FLASK)
+
 from flask import Flask, render_template, Blueprint
+# IMPORTA O BLUEPRINT DECLARADO EM ./modulos/dockerm.py
 from modulos.dockerm import docker
 
 app = Flask(__name__)
+# USAMOS BLUEPRINT PARA CHAMAR OUTROS MODULOS (ESTA E A FORMA QUE O FLASK MODULARIZA)
 app.register_blueprint(docker)
 
 @app.route("/")
--- dashboard/modulos/dockerm.py
+++ dashboard/modulos/dockerm.py
@@ -1,12 +1,17 @@
 from flask import render_template, Blueprint, redirect
 from docker import DockerClient
 
+# NESTE EXEMPLO NOS JA DECLARAMOS O url_prefix, POR ISSO O PARAMETRO DO @docker.route() está vazio
 docker = Blueprint("docker_routes", __name__, url_prefix="/docker")
 con = DockerClient("tcp://127.0.0.1:2376")
 
+# ESTA ROTA JA TEM A RAIZ PADRAO COMO /docker (DECLARADO ACIMA)
 @docker.route("")
+# NOME DA FUNCAO
 def get_containers():
+    # OBTEM A LISTA DE CONTAINERS NO OBJETO containers (ESTE OBJETO POSSUI FUNCOES PROPRIAS DO MODULO DOCKER, E SERAO USADOS NA PAGINA docker.html)
     containers = con.containers.list(all=True)
+    # ENVIA O RESULTADO PARA A PAGINA docker.html NA FORMA DA VARIAVEL ctrs
     return render_template("docker.html", ctrs=containers)
 
 #TODAS AS ROTAS DESTE PREFIXO COMECAM COM "/docker", LOGO A ROTA ABAIXO É PARA /docker/start
