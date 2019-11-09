6421c133d4d11817317d642c49f3a88f54b86145
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-11-09 11:08:32 -0200

    added start/stop container funcionality

--- dashboard/modulos/dockerm.py
+++ dashboard/modulos/dockerm.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect
 from docker import DockerClient
 
 docker = Blueprint("docker_routes", __name__, url_prefix="/docker")
@@ -8,3 +8,14 @@ con = DockerClient("tcp://127.0.0.1:2376")
 def get_containers():
     containers = con.containers.list(all=True)
     return render_template("docker.html", ctrs=containers)
+
+#TODAS AS ROTAS DESTE PREFIXO COMECAM COM "/docker", LOGO A ROTA ABAIXO É PARA /docker/start
+@docker.route("/start/<short_id>")
+def start_containers(short_id):
+    con.containers.get(short_id).start()
+    return redirect("/docker")
+
+@docker.route("/stop/<short_id>")
+def stop_containers(short_id):
+    con.containers.get(short_id).stop()
+    return redirect("/docker")
