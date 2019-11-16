7cfdaed11fd2fd8e52907afa4fba588db0237d97
jean <jeancarlosdearaujo@gmail.com>
2019-11-16 17:39:05 -0200

    editing job on jenkins

--- dashboard/modulos/jenkinsm.py
+++ dashboard/modulos/jenkinsm.py
@@ -1,15 +1,28 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, request
 from jenkins import Jenkins as jenkins_con
+from time import sleep
 
 jenkins = Blueprint("jenkins_routes", __name__, url_prefix="/jenkins")
-#CHANGE TO JENKINS MACHINE NAME/IP
 con = jenkins_con('http://localhost:8080/', username='jenkins', password='jenkins')
 
 @jenkins.route("")
 def index():
+    #CHANGE TO JENKINS MACHINE NAME/IP
     jobs = con.get_all_jobs()
     return render_template("jenkins.html", jobs=jobs)
 
+@jenkins.route("/build/<string:name>")
+def jenkins_build(name):
+    con.build_job(name)
+    sleep(5)
+    return redirect("/jenkins")
+
 @jenkins.route("/update/<string:name>")
 def jenkins_update(name):
-    return render_template("jenkins_update.html")
+    xml = con.get_job_config(name)
+    return render_template("jenkins_update.html", job_name=name, xml=xml)
+
+@jenkins.route("/reconfig/<string:name>", methods=["POST"])
+def jenkins_reconfigure(name):
+    con.reconfig_job(name, request.form["xml"])
+    return redirect("/jenkins")
